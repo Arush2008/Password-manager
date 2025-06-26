98f76622ba87f343b9aff66091f825197eaf2bae
HeroicP <23779@burnside.school.nz>
2025-06-26 21:21:19 +1200

    Layout is done

--- views.py
+++ views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request
 
-views = Blueprint(__name__, "views")
+views = Blueprint('views', __name__)
 
-@views.route("/")
-def home():
-    return render_template("index.html")
+@views.route('/')
+def index():
+    return render_template('index.html')
+
+@views.route('/vault')
+def vault():
+    category = request.args.get('category', 'all')
+    return render_template('vault.html', category=category)
